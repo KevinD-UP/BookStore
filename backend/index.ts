bdbe444806e23036a35a590f3e8f133a6d15d6d3
Kévin DANG <kdang@pitchy.fr>
2022-08-04 00:59:47 +0200

    Init project

---
+++ backend/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { container } from "./src/IoC/container";
+import { InversifyExpressServer } from "inversify-express-utils";
+import cors from "cors"
+import bodyParser from "body-parser";
+import "./src/Presentation/UserController";
+
+const port = process.env.PORT || 8000 ;
+
+// create server
+let server = new InversifyExpressServer(container);
+server.setConfig((app) => {
+    app.use(cors())
+    // add body parser
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+});
+
+let app = server.build();
+app.listen(port)
